a1d8fb967b8a248c8a8103315d2bf745b8ca15ed
madeline_park2 <madelinepark@ymail.com>
2024-04-19 08:24:37 -0700

    fixed typo

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -27,14 +27,13 @@ function MyApp() {
   }
 
   function postUser(person) {
-    const promise = fetch("Http://localhost:8000/users", {
+    const promise = fetch("http://localhost:8000/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(person),
     });
-
     return promise;
   }
 
