ddb70dd1583126a89a5e3e184f3f21f8534f59e4
madeline_park2 <madelinepark@ymail.com>
2024-04-18 21:55:47 -0700

    working

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -22,6 +22,18 @@ function MyApp() {
     return promise;
   }
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
